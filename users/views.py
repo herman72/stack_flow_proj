b63e060eab4925240cfe631aefb7855dd1a36fa8
herman72 <m.sherafati7@gmail.com>
2023-10-03 18:21:32 +0300

    added login views

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout, authenticate, login
 
@@ -8,11 +8,39 @@ from users.models import User
 
 # Create your views here.
 def user_register_view(request):
-    pass
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You registered successfully.')
+                return redirect('home')
+        else:
+            messages.warning(request, 'Register unsuccessfully.')
+
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
 
 
 def user_login_view(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('home')
+        else:
+            messages.warning(request, 'Login unsuccessful.')
+
+    return render(request, 'registration/login.html')
 
 
 def user_logout_view(request):
