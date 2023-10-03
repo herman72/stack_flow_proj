328554b5311d386e7a3f986821abe1d0910f7f5d
herman72 <m.sherafati7@gmail.com>
2023-10-03 15:04:32 +0300

    added clean for username and email to regist user

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import User
 
@@ -8,8 +9,23 @@ class UserUpdateForm(forms.ModelForm):
 
 
 class UserRegisterForm(UserUpdateForm):
-    pass
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
 
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirm_password', 'phone_number', 'national_id']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("This email is already exist!")
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError("This username is already exist!")
+        return username
 
 class UserLoginForm(forms.Form):
     pass
