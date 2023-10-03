a5a774cd8ebc4ee961f11de381bf20a79d2a9bc2
herman72 <m.sherafati7@gmail.com>
2023-10-03 03:12:59 +0300

    user model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    phone_number = models.CharField(max_length=11, unique=True)
+    national_id = models.CharField(max_length=10, unique=True)
 
-    pass
+    def __str__(self):
+        return self.username
+
