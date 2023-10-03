975d656df92ff8f3f562c54b9a0f335a90eb121c
herman72 <m.sherafati7@gmail.com>
2023-10-03 15:24:04 +0300

    added question form

--- questions/forms.py
+++ questions/forms.py
@@ -4,7 +4,12 @@ from .models import Question, Answer, Tag
 
 
 class QuestionForm(forms.ModelForm):
-    pass
+    title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    body = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
+    tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'checkbox'}),
+    )
 
 
 class AnswerForm(forms.ModelForm):
